2c311ab673f3c22cf311df8f2d51bbac3f60cf11
Bugra <bugraa@live.com>
2012-12-02 14:13:40 -0500

    Change in mean square error, needs to be checked

--- mean_square_error.m
+++ mean_square_error.m
@@ -1,9 +1,4 @@
 function mse = mean_square_error(im1, im2)
 % Takes im1 and im2 and computes the mean square error
 diff = im1 - im2;
-mse = 0;
-for ii = 1:size(diff,1)
-    for jj = 1:size(diff,2)
-        mse = mse + diff(ii,jj)^2;
-    end
-end
+mse = sum(sum(diff.^2));
