e1aee84cbfae98da77e6f16a623facf526338da0
Bugra <bugraa@live.com>
2012-12-04 10:52:52 -0500

    Adding lots of inline comments, needs to write the algorithm in both matlab and project description file

--- read_color_raw.m
+++ read_color_raw.m
@@ -5,29 +5,30 @@ fclose(fid);
 
 r = raw_image_size(1); c = raw_image_size(2);
 
-% Initialization of color fields
-R = zeros(r,c);
-G = zeros(r,c);
-B = zeros(r,c);
+% Initialization of color images for Red, Green and Blue
+Red = zeros(r,c);
+Green = zeros(r,c);
+Blue = zeros(r,c);
 
-% Initialization of the RGB image
+% Initialization of the RGB image, which will be returned by this
+% function
 img = zeros(r,c,3);
 
 % RGB values are read in the same order they are provided 
 for ii = 1:r
     for jj = 1:c
-        R(ii,jj)=im((ii-1)*(3*c)+(jj-1)*3+1); 
-        G(ii,jj)=im((ii-1)*(3*c)+(jj-1)*3+2);
-        B(ii,jj)=im((ii-1)*(3*c)+(jj-1)*3+3);
+        Red(ii,jj)=im((ii-1)*(3*c)+(jj-1)*3+1); 
+        Green(ii,jj)=im((ii-1)*(3*c)+(jj-1)*3+2);
+        Blue(ii,jj)=im((ii-1)*(3*c)+(jj-1)*3+3);
     end
 end
-% Construction of RGB image, in 3-D,
+% Construction of RGB image, in 3-D, 3-Dimensional array,
 % 3rd dimension is for R, G, B values respectively.
 for ii = 1:r
     for jj = 1:c
-        img(ii,jj,1)=R(ii,jj);
-        img(ii,jj,2)=G(ii,jj);
-        img(ii,jj,3)=B(ii,jj);
+        img(ii,jj,1)=Red(ii,jj);
+        img(ii,jj,2)=Green(ii,jj);
+        img(ii,jj,3)=Blue(ii,jj);
     end
 end
 imshow(uint8(img));
